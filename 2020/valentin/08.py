f677d135a951dbeecb7598009c86c9c9f3136cf9
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2020-12-10 18:22:01 +0100

    Adds 08.py (part 2) – double-trouble solution with roll-back and state restoration

--- 2020/valentin/08.py
+++ 2020/valentin/08.py
@@ -1,7 +1,7 @@
 import re
 import sys
 import time
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
 
 # Handheld Halting
 #
@@ -13,6 +13,8 @@ from typing import List, Tuple
 # GLOBALS
 accumulator: int = 0
 program: List[Tuple[str, int]] = []
+instructions_executed: List[int] = []
+hit_detector: List[bool]
 
 # Run the program until it reaches the same instruction a second time.
 # Terminate and print accumulator value
@@ -55,6 +57,53 @@ def execute(instruction_idx: int) -> int:
         raise ValueError(f"Unknown instruction: {ins}")
 
 
+def roll_back(invalid_changes: List[int]) -> int:
+    """
+    Roll back to the state before the last jmp or nop will be executed.
+
+    returns: pointer to the last jmp or nop as the next instruction to execute
+    """
+
+    global instructions_executed
+    global program
+    global accumulator
+    global hit_detector
+
+    pointer: int
+    while True:
+        # Always roll back pointer and remove executed instruction from list
+        pointer = instructions_executed.pop(-1)
+        hit_detector[pointer] = False
+        ins: str = program[pointer][0]
+
+        if ins == "acc":
+            # Reverse accumulation
+            accumulator -= program[pointer][1]
+            continue
+
+        # ins is "jmp" or "nop"
+
+        # Only stop if we have not tried to change this position before
+        if pointer not in invalid_changes:
+            break
+
+    # Hit the state before the last unchanged jmp or nop
+
+    # The instruction currently pointed to was rolled back
+    return pointer
+
+
+def swap_jmp_nop(pointer: int):
+    """ Swap the instruction the pointer points to. """
+
+    ins: Tuple[str, int] = program[pointer]
+
+    if ins[0] == "jmp":
+        program[pointer] = "nop", ins[1]
+    else:
+        program[pointer] = "jmp", ins[1]
+
+
 t0 = time.perf_counter()
 
 with open(sys.argv[1], "r") as f:
@@ -62,16 +111,51 @@ with open(sys.argv[1], "r") as f:
         instruction: Tuple[str, int] = parse_instruction(l)
         program.append(instruction)
 
+hit_detector = [False] * len(program)
+
 t1 = time.perf_counter()
 
 instruction_index: int = 0
-hit_counter: List[bool] = [False] * len(program)
+code_changed: bool = False  # Was an instruction changed, and if yes, which?
+past_changes: List[int] = []  # Changed indices that did not fix the infinite loop
+
+state: Dict[str, Any] = {}
 
 while True:
-    if hit_counter[instruction_index]:
+    # Successfull termination
+    if instruction_index >= len(program):
         break
 
-    hit_counter[instruction_index] = True
+    if hit_detector[instruction_index]:
+        if code_changed:
+            # Revert change again
+            swap_jmp_nop(past_changes[-1])
+            code_changed = False
+
+            # Go back to state before the change was tried
+            instruction_index = state["instruction_index"]
+            instructions_executed = state["instructions_executed"]
+            hit_detector = state["hit_detector"]
+            accumulator = state["accumulator"]
+
+        # Roll back until first untried jmp or nop
+        instruction_index = roll_back(past_changes)
+        # Change position
+        swap_jmp_nop(instruction_index)
+        code_changed = True
+        past_changes.append(instruction_index)
+
+        # Save state to later revert if unsuccessful
+        state["accumulator"] = accumulator
+        state["instruction_index"] = instruction_index
+        state["instructions_executed"] = instructions_executed.copy()
+        state["hit_detector"] = hit_detector.copy()
+        # The program is changed with the swap function
+
+        # Continue running
+
+    instructions_executed.append(instruction_index)
+    hit_detector[instruction_index] = True
     instruction_index = execute(instruction_index)
 
 t2 = time.perf_counter()
