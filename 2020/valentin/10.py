6d2add44c170ff40427d5a6723af489427911728
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2020-12-11 21:57:24 +0100

    Adds 10.py (part 2) – naive solution

--- 2020/valentin/10.py
+++ 2020/valentin/10.py
@@ -21,42 +21,75 @@ with open(sys.argv[1], "r") as f:
     for l in f:
         adapters.append(int(l[:-1]))
 
-# Add end (device = max+3) to list
-adapters.append(max(adapters) + 3)
+# end / device = max+3
+device: int = max(adapters) + 3
 
 t1 = time.perf_counter()
 
 adapters.sort()
 
+# Part 1
+
+
+def count_joltage_steps(adapters: List[int]) -> int:
+    deltas: Counter = Counter({x: 0 for x in [1, 2, 3]})
+    last_adapter: int = 0
+
+    for next_adapter in adapters:
+        delta: int = next_adapter - last_adapter
+        if delta not in deltas:
+            raise ValueError(f"Delta of {delta} found: {next_adapter} - {last_adapter}")
+
+        deltas[delta] += 1
+        last_adapter = next_adapter
+
+    return deltas[1] * deltas[3]
+
+
+# result: int = count_joltage_steps(adapters + [device])
+
+# B) Find all possible chains that connect the socket to the device
+#    and count the number of distinct chains.
+
+# Part 2
+
+
+def count_distinct_chains(adapters: List[int], start: int, end: int) -> int:
+    # Recurse into possible steps and sum up
+    chain_count: int = 0
+
+    for i in range(len(adapters)):
+        adp: int = adapters[i]
+        delta: int = adp - start
+
+        if delta <= 0:
+            raise ValueError("Invalid step")
+        elif delta > 3:
+            # Input list is sorted, so we have reached the end
+            break
+
+        # Valid if delta in [1, 3]
+        chain_count += count_distinct_chains(adapters[i + 1 :], start=adp, end=end)
+
+    # Recursion end: List is empty
+    if not adapters and ((end - start) in [1, 2, 3]):
+        return 1
+
+    return chain_count
+
+
+chain_count: int = count_distinct_chains(adapters, start=0, end=device)
+
 t2 = time.perf_counter()
-
-deltas: Counter = Counter({x: 0 for x in [1, 2, 3]})
-last_adapter: int = 0
-
-for next_adapter in adapters:
-    delta: int = next_adapter - last_adapter
-    if delta not in deltas:
-        raise ValueError(f"Delta of {delta} found: {next_adapter} - {last_adapter}")
-
-    deltas[delta] += 1
-    last_adapter = next_adapter
-
-t3 = time.perf_counter()
-
-result: int = deltas[1] * deltas[3]
-
-t4 = time.perf_counter()
 
 
 from util import tf
 
 print(
-    f"Part 1: Result = {result}\n"
+    f"Number of distinct chains = {chain_count}\n"
     f"\n"
     f"Parse file and collect adapters: {tf(t1-t0)}\n"
-    f"Sort: {tf(t2-t1)}\n"
-    f"Count deltas: {tf(t3-t2)}\n"
-    f"Get result: {tf(t4-t3)}\n"
+    f"Count chains: {tf(t2-t1)}\n"
     f"=====\n"
-    f"Total: {tf(t4-t0)}"
+    f"Total: {tf(t2-t0)}"
 )
