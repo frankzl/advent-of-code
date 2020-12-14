77be1ffc5d4f9245261291a1506916d81a262a4d
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2020-12-14 16:20:30 +0100

    Starts 13.py (part 2) – WIP

--- 2020/valentin/13.py
+++ 2020/valentin/13.py
@@ -1,6 +1,8 @@
 import sys
 import time
-from typing import List, Tuple
+from typing import List, Tuple, Union, cast
+
+# pylint: disable=unsubscriptable-object
 
 # Shuttle Search
 #
@@ -20,10 +22,7 @@ t1 = time.perf_counter()
 # Part 1: (ID of earliest bus that we can take) * (number of minutes we need to wait for the bus)
 
 
-def find_earliest_and_time() -> Tuple[int, int]:
-    depart_time: int = int(lines[0])
-    buses: List[int] = [int(b) for b in lines[1].split(",") if b != "x"]
-
+def find_earliest_and_time(depart_time: int, buses: List[int]) -> Tuple[int, int]:
     current_time: int = depart_time
     while True:
         for bus in buses:
@@ -36,20 +35,63 @@ def find_earliest_and_time() -> Tuple[int, int]:
 
 earliest_bus_id: int
 time_to_wait: int
-earliest_bus_id, time_to_wait = find_earliest_and_time()
+earliest_bus_id, time_to_wait = find_earliest_and_time(
+    depart_time=int(lines[0]), buses=[int(b) for b in lines[1].split(",") if b != "x"]
+)
 
 t2 = time.perf_counter()
+
+# Part 2: Find earliest timestamp so that each tick the next bus from the list departs (x is empty)
+
+t3 = time.perf_counter()
+
+
+def find_first_valid_timestamp() -> int:
+    buses_and_skips: List[Union[int, str]] = [
+        int(b) if b != "x" else b for b in lines[1].split(",")
+    ]
+
+    buses_with_offsets: List[Tuple[int, int]] = [
+        (int(b), i) for (i, b) in enumerate(buses_and_skips) if b != "x"
+    ]
+
+    # Idea: Go through buses, find earliest time for first, then second ... and restart if none is found.
+    bus: int
+    offset: int
+    first_valid_timestamp: int = 0
+    while True:
+        try:
+            for bus, offset in buses_with_offsets:
+                earliest_bus_id, time_to_wait = find_earliest_and_time(
+                    depart_time=first_valid_timestamp, buses=[bus]
+                )
+                assert earliest_bus_id == bus  # remove later
+
+                if time_to_wait == offset:
+                    continue
+
+                # time_to_wait > offset means the schedule is invalid
+                raise RuntimeError()
+
+            # We found a valid slot
+            return first_valid_timestamp
+        except RuntimeError:
+            first_valid_timestamp += 1
+
+
+first_valid_timestamp: int = find_first_valid_timestamp()
 
 
 from util import tf
 
 print(
-    f"Result = {earliest_bus_id * time_to_wait}\n"
-    f"Earliest bus = {earliest_bus_id}\n"
-    f"Time to wait = {time_to_wait}\n"
+    f"Part 1: {earliest_bus_id * time_to_wait}\n"
+    f" (Earliest bus: {earliest_bus_id}, wait time: {time_to_wait}\n"
+    f"Part 2: {first_valid_timestamp}\n"
     f"\n"
     f"Parse file: {tf(t1-t0)}\n"
-    f"Find earliest and time: {tf(t2-t1)}\n"
+    f"Part 1: Find earliest and time: {tf(t2-t1)}\n"
+    f"Part 2: Find earliest valid timestamp: {tf(t3-t2)}\n"
     f"=====\n"
-    f"Total: {tf(t2-t0)}"
+    f"Total: {tf(t3-t0)}"
 )
